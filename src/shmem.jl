c5568daea13eb653ca52be245b2c3a18a759dce8
Valentin Churavy <v.churavy@gmail.com>
2019-01-31 14:34:17 -0500

    add first version of shmem macro

--- src/shmem.jl
+++ src/shmem.jl
@@ -1,2 +1,10 @@
-macro shmem()
+macro shmem(T, Dims)
+    dims = Dims.args
+    quote
+        if $iscpu(__DEVICE)
+            $MArray{Tuple{$(dims...)}, $T}(undef)
+        else
+            @cuStaticSharedMem($T, $Dims)
+        end
+    end
 end
