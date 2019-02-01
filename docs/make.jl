247c966292508b7e68e185196f4deb05f9c21f1e
Valentin Churavy <v.churavy@gmail.com>
2019-02-01 15:54:09 -0500

    fix setup

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, GPUifyLoops
 
 makedocs(
     modules = [GPUifyLoops],
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    format = :html,
     sitename = "GPUifyLoops.jl",
     pages = [
         "Home"    => "index.md",
