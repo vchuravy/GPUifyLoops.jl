080ec8138ba3d8502f0182ad29fd37065fe8433e
Valentin Churavy <v.churavy@gmail.com>
2019-02-18 17:15:34 -0500

    steal GPUArrays lmem macro

--- src/shmem.jl
+++ src/shmem.jl
@@ -1,3 +1,27 @@
+"""
+Creates a local static memory shared inside one block.
+Equivalent to `__local` of OpenCL or `__shared__ (<variable>)` of CUDA.
+"""
+macro LocalMemory(state, T, N)
+    id = (shmem_counter[] += 1)
+    quote
+        lémem = LocalMemory($(esc(state)), $(esc(T)), Val($(esc(N))), Val($id))
+        AbstractDeviceArray(lémem, $(esc(N)))
+    end
+end
+
+export @LocalMemory
+
+"""
+Creates a block local array pointer with `T` being the element type
+and `N` the length. Both T and N need to be static! C is a counter for
+approriately get the correct Local mem id in CUDAnative.
+This is an internal method which needs to be overloaded by the GPU Array backends
+"""
+function LocalMemory(state, ::Type{T}, ::Val{N}, ::Val{C}) where {N, T, C}
+    error("Not implemented")
+end
+
 macro shmem(T, Dims)
     dims = Dims.args
     esc(quote
