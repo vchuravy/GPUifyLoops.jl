24e15f977b73b11623d81467acd1dfb7a92d6dea
Lucas C Wilcox <lucas@swirlee.com>
2019-03-06 15:02:24 -0600

    Add docs for contextualize

--- src/context.jl
+++ src/context.jl
@@ -71,7 +71,7 @@ end
 ###
 
 # libdevice.jl
-for f in (:cos, :cospi, :sin, :sinpi, :tan,
+const cudafuns = (:cos, :cospi, :sin, :sinpi, :tan,
           :acos, :asin, :atan,
           :cosh, :sinh, :tanh,
           :acosh, :asinh, :atanh,
@@ -81,11 +81,48 @@ for f in (:cos, :cospi, :sin, :sinpi, :tan,
           :signbit, :abs,
           :sqrt, :cbrt,
           :ceil, :floor,)
+for f in cudafuns
     @eval function Cassette.overdub(ctx::Ctx, ::typeof(Base.$f), x::Union{Float32, Float64})
         @Base._inline_meta
         return CUDAnative.$f(x)
     end
 end
 
+"""
+    contextualize(::Dev, f)
+
+This contexualizes the function `f` for a given device type `Dev`.
+
+For the device `CUDA()`, `contextualize` replaces calls to math library
+functions.  For example, `cos`, `sin`, are replaced with `CUDAnative.cos`,
+`CUDAnative.sin`, respectively.
+
+The full list functions that are replaced is $cudafuns.
+
+# Examples
+```julia
+function kernel!(::Dev, A, f) where {Dev}
+    @setup Dev
+    @loop for i in (1:size(A,1); threadIdx().x)
+        A[i] = f(A[i])
+    end
+end
+
+g(x) = sin(x)
+kernel!(A::Array) = kernel!(CPU(), A, contextualize(CPU(), g))
+kernel!(A::CuArray) =
+    @cuda threads=length(A) kernel!(CUDA(), A, contextualize(CUDA(), g))
+
+a = rand(Float32, 1024)
+b, c = copy(a), CuArray(a)
+
+kernel!(b)
+kernel!(c)
+
+@assert g.(a) ≈ b
+@assert g.(a) ≈ c
+```
+"""
+function contextualize end
 contextualize(::CUDA, f::F) where F = (args...) -> Cassette.overdub(ctx, f, args...)
 contextualize(::CPU,  f::F) where F = (args...) -> f(args...)
