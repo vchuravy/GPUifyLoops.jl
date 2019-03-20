b5d3aca17ef7c3f5c02ae9c15d0f6f61a69aca5c
Lucas C Wilcox <lucas@swirlee.com>
2019-03-20 16:30:27 -0500

    Allow @shmem without @cuStaticSharedMem defined
    This is the last step needed to address #3.

--- src/shmem.jl
+++ src/shmem.jl
@@ -1,10 +1,17 @@
+shmem_id = 0
+
 macro shmem(T, Dims)
+    global shmem_id
+    id = shmem_id::Int += 1
+
     dims = Dims.args
     esc(quote
         if !$isdevice()
             $MArray{Tuple{$(dims...)}, $T}(undef)
         else
-            @cuStaticSharedMem($T, $Dims)
+            len = prod($Dims)
+            ptr = CUDAnative._shmem(Val($id), $T, Val(len))
+            CUDAnative.CuDeviceArray($Dims, CUDAnative.DevicePtr{$T, CUDAnative.AS.Shared}(ptr))
         end
     end)
 end
