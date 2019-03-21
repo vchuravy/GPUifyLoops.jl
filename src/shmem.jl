d58d371a7005f6de1bc7e6e67dbce623e364a72c
Lucas C Wilcox <lucas@swirlee.com>
2019-03-21 18:48:22 -0500

    Try this

--- src/shmem.jl
+++ src/shmem.jl
@@ -1,5 +1,19 @@
 shmem_id = 0
 
+@init @require CUDAnative="be33ccc6-a3ff-5ff2-a52e-74243cff1e17" begin
+    using .CUDAnative
+
+    @inline function _shmemcu(::Type{T}, ::Val{N}, ::Val{J}) where {T, N, J}
+      len = prod(N)
+      ptr = CUDAnative._shmem(J, T, len)
+      CUDAnative.CuDeviceArray(N, CUDAnative.DevicePtr{T, CUDAnative.AS.Shared}(ptr))
+    end
+end
+
+_shmem(::Type{T}, ::Val{N}, ::Val{J}) where {T, N, J} = nothing
+
+Cassette.overdub(ctx::Ctx, ::typeof(_shmem), args...) = _shmemcu(args...)
+
 macro shmem(T, Dims)
     global shmem_id
     id = shmem_id::Int += 1
@@ -9,9 +23,7 @@ macro shmem(T, Dims)
         if !$isdevice()
             $MArray{Tuple{$(dims...)}, $T}(undef)
         else
-            len = prod($Dims)
-            ptr = $CUDAnative._shmem(Val($id), $T, Val(len))
-            $CUDAnative.CuDeviceArray($Dims, $CUDAnative.DevicePtr{$T, $CUDAnative.AS.Shared}(ptr))
+            _shmem(Val($T), Val(Dims), Val($id))
         end
     end)
 end
