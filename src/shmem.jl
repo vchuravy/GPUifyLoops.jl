a4dd19a21cece15c84b818d62a13ff36d5597287
Valentin Churavy <v.churavy@gmail.com>
2019-03-21 22:28:45 -0400

    maybe this way?

--- src/shmem.jl
+++ src/shmem.jl
@@ -1,21 +1,20 @@
-shmem_id = 0
-
-@init @require CUDAnative="be33ccc6-a3ff-5ff2-a52e-74243cff1e17" begin
-    using .CUDAnative
-
-    @inline function _shmemcu(::Type{T}, ::Val{N}, ::Val{id}) where {T, N, id}
-      ptr = CUDAnative._shmem(Val(id), T, Val(N))
-      CuDeviceArray(N, DevicePtr{T, CUDAnative.AS.Shared}(ptr))
-    end
-    _shmemcuarray(A::CUDAnative.CuDeviceArray, shape) = CUDAnative.CuDeviceArray(shape, pointer(A))
-end
 
 _shmem(::Type{T}, ::Val{N}, ::Val{id}) where {T, N, id} = nothing
 _shmemarray(A, shape) = nothing
 
-Cassette.overdub(ctx::Ctx, ::typeof(_shmem), args...) = _shmemcu(args...)
-Cassette.overdub(ctx::Ctx, ::typeof(_shmemarray), args...) = _shmemcuarray(args...)
+@init @require CUDAnative="be33ccc6-a3ff-5ff2-a52e-74243cff1e17" begin
+    using .CUDAnative
 
+    @inline function Cassette.overdub(ctx::Ctx, ::typeof(_shmem), ::Type{T}, ::Val{N}, ::Val{id}) where {T, N, id}
+        ptr = CUDAnative._shmem(Val(id), T, Val(N))
+        CuDeviceArray(N, DevicePtr{T, CUDAnative.AS.Shared}(ptr))
+    end
+    @inline function Cassette.overdub(ctx::Ctx, ::typeof(_shmemarray), A::CUDAnative.CuDeviceArray, shape)
+        CUDAnative.CuDeviceArray(shape, pointer(A))
+    end
+end
+
+shmem_id = 0
 macro shmem(T, Dims)
     global shmem_id
     id = shmem_id::Int += 1
